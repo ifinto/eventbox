acfac76d0c1a9eee8463435ee6878fb2a5aaf58a
Andrey M <andrey.malyniak@gmail.com>
2016-11-13 21:35:15 +0200

    parser updated

--- parse.js
+++ parse.js
@@ -27,38 +27,39 @@ var childArgs = [
 ]
 
 new Promise((resolve, reject) => {
-  childProcess.execFile('./phantomjs/bin/phantomjs', childArgs, function(err, stdout, stderr) {
-    if (err) console.log('Error at phantom.js: ', err);
-    resolve(stdout)
-  })
-  // resolve(fs.readFileSync('vk.html', 'utf8'))
-})
-.then(function (content) {
+  // childProcess.execFile('./phantomjs/bin/phantomjs', childArgs, function(err, stdout, stderr) {
+  //   if (err) console.log('Error at phantom.js: ', err);
+  //   resolve(stdout)
+  // })
+  resolve(fs.readFileSync('vk.html', 'utf8'))
+}).then(function (content) {
   console.log('done parsing!')
   var content = fs.readFileSync('vk.html')
   let $ = cheerio.load(content, {decodeEntities: false})
   console.log('length', $('._post.post.page_block:not(.post_fixed)').length);
+  var posts = []
   $('._post.post.page_block:not(.post_fixed)').each((i, el) => {
-      var post = {}
-      var allHtml = $(el).html()
-      post.title = $(el).find('.wall_post_text .mem_link').first().html()
-      var text = $(el).find('.wall_post_text').html()
-      var thumbs = $(el).find('.page_post_sized_thumbs a').html()
-      text = text.replace(/<img.*?>/g, '')
-      text = text.replace(/<a.*?mem_link.*?\/a>/g, '')
-      text = text.replace(/<a.*?wall_post_more.*?\/a>/g, '')
-      text = text.replace(/<span style="display: none">/g, '')
-      text = text.replace(/<a href="\/feed.*/g, '')
-      text = text.replace(/onclick="return mentionClick\(this, event\)"/g, '')
-      text = text.replace(/onmouseover="mentionOver\(this\)"/g, '')
-      text = text.replace(/mention_id=".*?"/g, '')
-      text = text.replace(/<br.*?>/g, '\n')
-      var matches = text.match(/(.|\n)*((\Время:|\Когда:|\Цена:)(.|\n)*)/im)
+    var allHtml = $(el).html()
+    var post = {};
+    post.title = $(el).find('.wall_post_text .mem_link').first().html()
+    post.thumbs = $(el).find('.page_post_sized_thumbs a').html()
+    var text = $(el).find('.wall_post_text').html()
+    text = text.replace(/<img.*?>/g, '')
+     .replace(/<a.*?mem_link.*?\/a>/g, '')
+     .replace(/<a.*?wall_post_more.*?\/a>/g, '')
+     .replace(/<a href="\/away.php\?to=(.*?)&.*?"/g, '<a href="$1"')
+     .replace(/<span style="display: none">/g, '')
+     .replace(/<a href="\/feed.*/g, '')
+     .replace(/onclick="return mentionClick\(this, event\)"/g, '')
+     .replace(/onmouseover="mentionOver\(this\)"/g, '')
+     .replace(/mention_id=".*?"/g, '')
+     .replace(/<br.*?>/g, '\n')
 
-      if (!matches) return
+    var matches = text.match(/((.|\n)*)((\Время:|\Когда:|\Цена:)(.|\n)*)/im)
 
-      var info = matches[0]
-      var arr = info.split('\n')
+    if (!!matches) {
+      post.text = matches[1]
+      var arr = matches[3].split('\n')
       var detailsKeys = {
         'time':    /Время:(.*)/i,
         'where':   /Когда:(.*)/i,
@@ -76,11 +77,50 @@ new Promise((resolve, reject) => {
           }
         })
       })
-      console.log('*************************');
-      console.log(details);
-      console.log('*************************');
-      
-      // console.log(details)
-      console.log('======================')
+    } else {
+      post.text = text
+    }
+
+    posts.push(post)
+  })
+  return posts
+}).mapSeries(function (post) {
+  post.title = post.title || ''
+  post.guid = Math.floor(Math.random() * 1000000000)
+  post.name = post.title.replace(/\s+/g, '-').toLowerCase() + '-' + post.guid
+  post.date = new Date().toISOString().slice(0, 19).replace('T', ' ')
+  var query = insertSqlQuery('wp_posts', {
+    'post_author':        1,
+    'post_date':          post.date,
+    'post_content':       escapeQuotes(post.text),
+    'post_title':         escapeQuotes(post.title),
+    'post_status':        'publish',
+    'post_name':          post.guid,
+    'post_type':          'post'
+  })
+  return new Promise((resolve, reject) => {
+    mySqlConnection.query(query, function(err, rows, fields) {
+      if (err) throw err
+      console.log('MySQL query success; rows: ', rows.affectedRows)
+      resolve()
+    })
   })
 })
+
+function insertNewPost() {
+}
+
+function insertSqlQuery(table, params) {
+  var headers = []
+  var values = []
+
+  Object.keys(params).forEach(function (key) {
+    headers.push('`' +key +'`')
+    values.push('\'' +params[key] +'\'')
+  })
+  return 'INSERT INTO `' + table + '` ('+ headers.join(',') +') VALUES ('+ values.join(',') +')'
+}    
+
+function escapeQuotes(str) {
+  return str.replace(/'/g, "\\$&");
+} 
